3ceeda022d1cb05039e5da7ce617bd6b12d046ce
刘云霞 <liuyunxia@liuyunxiadeMac-mini.local>
2017-12-20 15:31:16 +0800

    修改

--- Libraries/Image/ImageBackground.js
+++ Libraries/Image/ImageBackground.js
@@ -62,30 +62,21 @@ class ImageBackground extends React.Component<$FlowFixMeProps> {
   };
 
   render() {
-    const {children, style, imageStyle, imageRef, ...props} = this.props;
+    // const {children, style, imageStyle, imageRef, ...props} = this.props;
+    const {children, style, imageStyle, contentStyle, imageRef, ...props} = this.props;
 
     return (
       <View style={style} ref={this._captureRef}>
         <Image
           {...props}
           style={[
-            StyleSheet.absoluteFill,
-            {
-              // Temporary Workaround:
-              // Current (imperfect yet) implementation of <Image> overwrites width and height styles
-              // (which is not quite correct), and these styles conflict with explicitly set styles
-              // of <ImageBackground> and with our internal layout model here.
-              // So, we have to proxy/reapply these styles explicitly for actual <Image> component.
-              // This workaround should be removed after implementing proper support of
-              // intrinsic content size of the <Image>.
-              width: style.width,
-              height: style.height,
-            },
             imageStyle,
           ]}
           ref={imageRef}
         />
-        {children}
+        <View style={[StyleSheet.absoluteFill, contentStyle]}>
+          {children}
+        </View>
       </View>
     );
   }
