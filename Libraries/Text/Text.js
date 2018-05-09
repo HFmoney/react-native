cfeea5a593d019df45fe0c70100acd114f2d9138
HFmoney <mk@hfmoney.com>
2018-05-09 17:27:21 +0800

    Update Text.js

--- Libraries/Text/Text.js
+++ Libraries/Text/Text.js
@@ -53,7 +53,7 @@ const Text = createReactClass({
   getDefaultProps(): Object {
     return {
       accessible: true,
-      allowFontScaling: true,
+      allowFontScaling: false,
       ellipsizeMode: 'tail',
     };
   },
